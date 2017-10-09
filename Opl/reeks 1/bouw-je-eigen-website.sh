7ce86a92f16537a532771b757379efda22863307
GertjanDS <32641897+GertjanDS@users.noreply.github.com>
2017-10-09 15:45:34 +0200

    Update bouw-je-eigen-website.sh

--- Opl/reeks 1/bouw-je-eigen-website.sh
+++ Opl/reeks 1/bouw-je-eigen-website.sh
@@ -1,4 +1,4 @@
 nano index.html
-wget [URL to dolphins.png]
+wget https://dodona.ugent.be/exercises/1319355284/media/dolphins.png
 chmod o+r index.html dolphins.png
 scp index.html dolphins.png [YOUR_USER_NAME]@webssh.ugent.be:WWW
