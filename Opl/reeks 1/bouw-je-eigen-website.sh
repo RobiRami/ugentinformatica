c09d9059226447f8e4c4646a0bcc4d47e5d09aae
tibovanheule <seventie@live.be>
2017-10-09 15:43:04 +0200

    Update bouw-je-eigen-website.sh

--- Opl/reeks 1/bouw-je-eigen-website.sh
+++ Opl/reeks 1/bouw-je-eigen-website.sh
@@ -1,4 +1,4 @@
 nano index.html
 wget [URL to dolphins.png]
 chmod o+r index.html dolphins.png
-scp index.html dolphins.png tgvheule@webssh.ugent.be:WWW
+scp index.html dolphins.png [YOUR_USER_NAME]@webssh.ugent.be:WWW
