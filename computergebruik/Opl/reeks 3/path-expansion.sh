d4f72bb42f0b3c2eb13c8192ba8ed454724cb0d8
tibovanheule <seventie@live.be>
2017-10-12 15:40:13 +0200

    Add files via upload

---
+++ computergebruik/Opl/reeks 3/path-expansion.sh
@@ -0,0 +1,34 @@
+### command 1 ###
+test*
+
+### command 2 ###
+test?
+
+### command 3 ###
+test[[:digit:]]
+
+### command 4 ###
+*[pq]
+### command 5 ###
+*.txt
+
+### command 6 ###
+?[[:upper:]]*
+
+### command 7 ###
+?????
+
+### command 8 ###
+[!at]*
+
+### command 9 ###
+first/*
+
+### command 10 ###
+first/*.txt
+
+### command 11 ###
+*/data/*
+
+### command 12 ###
+*/*/*.txt
