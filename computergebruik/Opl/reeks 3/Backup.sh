71bea5770d7be0e184d2b0c45c1b16c1e02e5e88
Wildst <32733861+Wildst@users.noreply.github.com>
2017-10-12 17:01:06 +0200

    Update Backup.sh

--- computergebruik/Opl/reeks 3/Backup.sh
+++ computergebruik/Opl/reeks 3/Backup.sh
@@ -1,2 +1,1 @@
-find -newermt "Oct 12" -type f -exec cp {} {}.BACKUP ";" //DATUM MOET ALTIJD AANGEPAST WORDEN
-//OF// find -mtime -0.5  -type f -exec cp {} {}.BACKUP ";" //FILTER OP DE LAATSTE 12 UUR; IPV 24 WAT -1 DOET
+find . -daystart -mtime -1 -type f -exec cp {} {}.BACKUP ';'
