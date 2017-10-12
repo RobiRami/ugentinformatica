aed9fe1b6973835934ba3e0e3a05791ac89da30b
Unknown <seventie@live.be>
2017-10-12 16:04:31 +0200

    file extension change
    change

--- computergebruik/Opl/reeks 3/Coderegels.sh
+++ computergebruik/Opl/reeks 3/Coderegels.sh
@@ -1,3 +1,4 @@
+#3 manieren
 find . -name '*.$extension' | xargs wc -l | tr -dc '1-9\n' | tail -n 1
 //OF//    find . -name '*.$extension' | xargs wc -l | tr -dc '[[:digit:]]\n' | tail -n 1
 //OF//    find . -name '*.$extension' -exec cat {} ';'| wc -l 
