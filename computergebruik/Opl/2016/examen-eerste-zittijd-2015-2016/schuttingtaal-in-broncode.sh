1a604e007a85a3cfb5468926d2c2dc2d1e979299
wddridde <32778534+wddridde@users.noreply.github.com>
2017-12-28 19:26:07 +0100

    Refactored schuttingtaal

--- computergebruik/Opl/2016/examen-eerste-zittijd-2015-2016/schuttingtaal-in-broncode.sh
+++ computergebruik/Opl/2016/examen-eerste-zittijd-2015-2016/schuttingtaal-in-broncode.sh
@@ -1,3 +1,59 @@
+Schuttingtaal (https://dodona.ugent.be/nl/courses/19/exercises/1839353126/)
+door Winnie
+
+#! /bin/bash
+
+#Error-message-functie
+function error() {
+        echo "Syntaxis: schuttingtaal [-f] [-c commit] <git-repo> <token> 1>&2"
+        exit 1
+}
+#Lokale variabelen: zoeken naar bestanden of naar aantal matches?
+bestanden=false
+#Andere versie van repository doorzoeken?
+versie=""
+
+while getopts ":fc:" opt ; do
+        case "$opt" in
+        f)bestanden=true
+                ;;
+        c)versie="$OPTARG"
+                ;;
+        \?)error
+        esac
+done
+shift $((OPTIND-1))
+
+if [ $# -ne 2 ] ; then #Te weinig/veel argumenten(Enkel repo-locatie en token nodig)
+        error
+fi
+dirnaam=$(echo "$1" | sed 's/\.git//' | sed 's/.*\/\([^/]*$\)/\1/')
+#Meegegeven link omzetten naar correcte naam voor directory
+if [ ! -d "$dirnaam" ] ; then
+        git clone "$1" "$dirnaam" #Bestaat nog niet in working directory
+        cd $dirnaam
+else #Verplaatsen naar de nieuwe repository
+        cd $dirnaam #Bestaat reeds, updaten naar recentste versie
+        git pull origin master > /dev/null
+fi
+if [ ! -z "$versie" ] ; then
+        git checkout -q "$versie"
+        if [ $? != 0 ] ; then #Meegegeven commit bestaat niet
+                echo "Pathspec $versie does not exist" 1>&2
+                exit 2
+        fi
+fi
+if [ "$bestanden" = true ] ; then
+        grep -rl "$2" | wc -l #Tel het aantal bestanden met l-vlag
+else                            #Alle bestanden doorzoeken via r-vlag(recursief)
+        grep -r "$2" | wc -l
+fi
+exit 0
+
+
+
+DEPRECATED niet zo'n goede code maar als legacy mag het blijven staan, was steun voor laatste vlag
+
 c="false"
 f="false"
 
